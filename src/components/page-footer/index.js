de3dfcb785e4d92a96506717563af0c569fae444
0w0i0n0g0 <83498372+0w0i0n0g0@users.noreply.github.com>
2022-04-21 00:03:55 +0900

    Add tossface footer

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -4,7 +4,7 @@ import './style.scss';
 function PageFooter({ author, githubUrl }) {
   return (
     <footer className="page-footer-wrapper">
-      <p className="page-footer">
+      <p className="page-footer" style={{padding: "50vh 0 10px 0"}}>
         © {new Date().getFullYear()}
         &nbsp;
         <a href={githubUrl}>{author}</a>
@@ -12,6 +12,9 @@ function PageFooter({ author, githubUrl }) {
         <a href="https://github.com/zoomKoding/zoomkoding-gatsby-blog">
           &nbsp;zoomkoding-gatsby-blog
         </a>
+      </p>
+      <p style={{padding: "0 0 20px 0"}}>
+      이 페이지에는 토스팀에서 제공한 토스페이스가 적용되어 있습니다.
       </p>
     </footer>
   );
