ad9baeeaa9bda76f3bd3aaa0428f621b09bb94d2
0w0i0n0g0 <83498372+0w0i0n0g0@users.noreply.github.com>
2022-04-23 00:15:00 +0900

    Add TOC highlight feature

--- src/components/post-content/index.js
+++ src/components/post-content/index.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './style.scss';
 
 function PostContent({ html }) {
+  function nearestNumber(array, target) {
+    var abs = 0;
+    var min = Number.MAX_SAFE_INTEGER;
+    var index = 0;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] - target > 100) {
+        continue;
+      }
+      abs = array[i] - target < 0 ? -(array[i] - target) : array[i] - target;
+      if (abs < min) {
+        min = abs;
+        index = i;
+      }
+    }
+    return index;
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
+
+  let currentY = 0;
+
+  const handleScroll = () => {
+    //run scroll event
+    if (Math.abs(currentY - window.scrollY) > 10) {
+      //toc active reset
+      let i = 0;
+      let tocAnchors = document
+        .getElementsByClassName('table-of-contents')
+        .item(0)
+        .querySelectorAll('a');
+      for (i = 0; i < tocAnchors.length; i++) {
+        tocAnchors[i].classList.remove('active');
+      }
+      currentY = window.scrollY;
+      let anchors = document.getElementsByClassName('anchor');
+      let anchorsY = [];
+      for (i = 0; i < anchors.length; i++) {
+        let forEach = anchors[i];
+        let absoluteY = window.pageYOffset + forEach.getBoundingClientRect().top;
+        anchorsY[i] = absoluteY;
+      }
+      let activeIndex = nearestNumber(anchorsY, currentY);
+      let activeHref = anchors[activeIndex].getAttribute('href');
+      document
+        .getElementsByClassName('table-of-contents')
+        .item(0)
+        .querySelector("[href='" + activeHref + "']").className = 'active';
+      return;
+    }
+
+    //ignore scroll event
+    if (Math.abs(currentY - window.scrollY) <= 10) {
+      return;
+    }
+  };
+
   return (
     <div className="post-content">
       <div className="markdown" dangerouslySetInnerHTML={{ __html: html }} />
