656f954a279b967c2826d020e411520308266fea
0w0i0n0g0 <83498372+0w0i0n0g0@users.noreply.github.com>
2022-05-06 11:19:13 +0900

    Turn off source maps

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,9 @@ module.exports = {
 
   plugins: [
     {
+      resolve: 'gatsby-plugin-no-sourcemaps',
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `assets`,
