3fe7ba300cc37bb3f920891694c11f9a085a7579
0w0i0n0g0 <0w0i0n0g0@gmail.com>
2024-07-28 20:24:28 +0900

    404 페이지 디자인 추가.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Layout from '../layout';
 import Seo from '../components/seo';
 
@@ -7,8 +6,8 @@ function NotFoundPage() {
   return (
     <Layout>
       <Seo title="404: Not found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p style={{fontWeight: "900", fontSize: "150px", marginTop: "30%", background : "linear-gradient(to right, #ff0085, #ffcf00)", backgroundClip : "text", WebkitTextFillColor : "transparent", display : "inline-block"}}>404</p>
+      <p style={{fontWeight: "900", fontSize: "20px", marginTop: "20px"}}>Not Found.</p>
     </Layout>
   );
 }
