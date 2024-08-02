cd17e4e3ceb15e82692a844ca1e586f9814a87e2
0w0i0n0g0 <0w0i0n0g0@gmail.com>
2024-08-02 11:01:47 +0900

    블로그 footer 디자인 수정.

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -4,17 +4,17 @@ import './style.scss';
 function PageFooter({ author, githubUrl }) {
   return (
     <footer className="page-footer-wrapper">
-      <p className="page-footer" style={{ padding: '50vh 0 10px 0' }}>
+      <p className="page-footer" style={{ whiteSpace: "nowrap" }}>
         © {new Date().getFullYear()}
         &nbsp;
-        <a href={githubUrl}>{author}</a>
+        <a href={githubUrl} target="_blank" rel="noreferrer noopener">{author}</a>
         &nbsp;powered by
-        <a href="https://github.com/zoomKoding/zoomkoding-gatsby-blog">
+        <a href="https://github.com/zoomKoding/zoomkoding-gatsby-blog" target="_blank" rel="noreferrer noopener">
           &nbsp;zoomkoding-gatsby-blog
         </a>
       </p>
-      <p style={{ padding: '0 0 20px 0' }}>
-        이 페이지에는 토스팀에서 제공한 토스페이스가 적용되어 있습니다.
+      <p style={{ padding: '0 0 20px 0', whiteSpace: "nowrap" }}>
+        이 페이지에는 토스팀에서 제공한 <a href='https://toss.im/tossface/copyright' target="_blank" rel="noreferrer noopener">토스페이스</a>가 적용되어 있습니다.
       </p>
     </footer>
   );
