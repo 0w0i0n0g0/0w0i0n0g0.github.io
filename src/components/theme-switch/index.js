870dccf38848dc3be2f790cdff7af3d7e5327770
0w0i0n0g0 <0w0i0n0g0@gmail.com>
2024-08-12 12:30:38 +0900

    다크모드 변경 버튼 수정.

--- src/components/theme-switch/index.js
+++ src/components/theme-switch/index.js
@@ -2,24 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { IconButton } from '@mui/material';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
-import { getValueFromLocalStorage, setValueToLocalStorage } from '../../utils/localStorage';
+import { setValueToLocalStorage } from '../../utils/localStorage';
 import './style.scss';
 
-function ThemeSwitch() {
-  const [isDarkMode, setIsDarkMode] = useState(getValueFromLocalStorage('isDarkMode'));
+function ThemeSwitch(props) {
+  const isDarkMode = props.isDarkMode;
+  const setIsDarkMode = props.setIsDarkMode;
+  const [svgColor, setSvgColor] = useState((isDarkMode ? '#ffcc33' : '#3366FF'));
 
   useEffect(() => {
     setValueToLocalStorage('isDarkMode', isDarkMode);
     document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
+
+    // change svg color
+    setSvgColor(isDarkMode ? '#ffcc33' : '#3366FF');
   }, [isDarkMode]);
 
   return (
     <div className="dark-mode-button-wrapper">
       <IconButton className="dark-mode-button" onClick={() => setIsDarkMode((isDark) => !isDark)}>
         {isDarkMode ? (
-          <LightModeIcon className="dark-mode-icon" fontSize="large" />
+          <LightModeIcon className="dark-mode-icon" fontSize="large" style={{fill: svgColor}} />
         ) : (
-          <DarkModeIcon className="dark-mode-icon" fontSize="large" />
+          <DarkModeIcon className="dark-mode-icon" fontSize="large" style={{fill: svgColor}} />
         )}
       </IconButton>
     </div>
