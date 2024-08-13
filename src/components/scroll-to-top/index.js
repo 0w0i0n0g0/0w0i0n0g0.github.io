d9fa4708ec48c440d3baa6effd4318f7ed220282
0w0i0n0g0 <0w0i0n0g0@gmail.com>
2024-08-13 09:41:27 +0900

    맨 위로 가기 버튼 수정.

--- src/components/scroll-to-top/index.js
+++ src/components/scroll-to-top/index.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Fab from '@mui/material/Fab';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import Zoom from '@mui/material/Zoom';
 import './style.scss';
 
-function ScrollToTop() {
+function ScrollToTop(props) {
+  const isDarkMode = props.isDarkMode;
+  const [svgColor, setSvgColor] = useState((isDarkMode ? '#ffffff' : '#232326'));
+
+  useEffect(() => {
+    setSvgColor(isDarkMode ? '#ffffff' : '#232326');
+  }, [isDarkMode]);
+
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 100,
   });
 
   const handleClick = (event) => {
-    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
-    if (anchor) {
-      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'})
   };
 
   return (
     <Zoom className="scroll-to-top-button-wrapper" in={trigger}>
       <div className="scroll-to-top-button" onClick={handleClick} role="presentation">
         <Fab className="scroll-to-top-button">
-          <ArrowUpwardIcon className="scroll-to-top-icon" fontSize="large" />
+          <ArrowUpwardIcon className="scroll-to-top-icon" fontSize="large" style={{fill: svgColor}} />
         </Fab>
       </div>
     </Zoom>
